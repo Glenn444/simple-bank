8ee19090a459f789b9296d26fd7196224a36dc2a
Glenn444 <gmark1586@gmail.com>
2026-03-31 12:59:46 +0300

    Check error type and set status code accordingly

--- api/account.go
+++ api/account.go
@@ -7,89 +7,95 @@ import (
 	db "github.com/Glenn444/banking-app/internal/database"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"github.com/shopspring/decimal"
 )
 
-type createAccountRequest struct{
-	Owner string `json:"owner" binding:"required"`
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
-
-func (server *Server) createAccount(ctx *gin.Context){
+func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
-	if err := ctx.ShouldBindJSON(&req);err != nil{
-		ctx.JSON(http.StatusBadRequest,errorResponse(err))
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner:    req.Owner,
 		Currency: req.Currency,
-		Balance: decimal.Zero,
+		Balance:  decimal.Zero,
 	}
 
-	acc,err := server.store.CreateAccount(ctx,arg)
-	if err != nil{
-		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+	acc, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		if pqError, ok := err.(*pq.Error); ok {
+			switch pqError.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+			}
+			//log.Println(pqError.Code.Name())
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK,acc)
+	ctx.JSON(http.StatusOK, acc)
 
 }
 
-type AccountId struct{
+type AccountId struct {
 	ID string `uri:"id" binding:"required,uuid"`
 }
 
-func (server *Server)getAccountById(ctx *gin.Context){
+func (server *Server) getAccountById(ctx *gin.Context) {
 
 	var accountId AccountId
-	
-	if err := ctx.ShouldBindUri(&accountId); err != nil{
-		ctx.JSON(http.StatusBadRequest,errorResponse(err))
+
+	if err := ctx.ShouldBindUri(&accountId); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	argId,_ := uuid.Parse(accountId.ID)
-	acc,err := server.store.GetAccount(ctx,argId)
-	if err != nil{
-		if err == sql.ErrNoRows{
-			ctx.JSON(http.StatusNotFound,errorResponse(err))
+	argId, _ := uuid.Parse(accountId.ID)
+	acc, err := server.store.GetAccount(ctx, argId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK,acc)
+	ctx.JSON(http.StatusOK, acc)
 }
 
-
-type listAllAccountsParams struct{
-	PageNum int32 `form:"page_num" binding:"required,min=1"` //offset
+type listAllAccountsParams struct {
+	PageNum  int32 `form:"page_num" binding:"required,min=1"`         //offset
 	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"` //limit
 }
 
-func (server *Server)listAllAccounts(ctx *gin.Context){
+func (server *Server) listAllAccounts(ctx *gin.Context) {
 	var req listAllAccountsParams
-	
-	if err := ctx.ShouldBindQuery(&req); err != nil{
-		ctx.JSON(http.StatusBadRequest,errorResponse(err))
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
 	arg := db.ListAccountsParams{
-		Limit: req.PageSize,
+		Limit:  req.PageSize,
 		Offset: (req.PageNum - 1) * req.PageSize,
 	}
-	
-	accs,err := server.store.ListAccounts(ctx,arg)
-	if err != nil{
-		ctx.JSON(http.StatusInternalServerError,errorResponse(err))
+
+	accs, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK,accs)
+	ctx.JSON(http.StatusOK, accs)
 }
